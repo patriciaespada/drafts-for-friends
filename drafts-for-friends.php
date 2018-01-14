7bccd9bc890f6dd2680764bc4cbb986d725c9de4
titiespada <titiespada@16f77f2e-bb1b-4951-a0ee-c5cc7ba034af>
2018-01-14 23:33:56 +0000

    Added the documentation for each method and the correspondent method visibility.
    git-svn-id: https://vip-review-svn.wordpress.com/trials/titiespada@29727 16f77f2e-bb1b-4951-a0ee-c5cc7ba034af

--- drafts-for-friends.php
+++ drafts-for-friends.php
@@ -1,20 +1,29 @@
 <?php
-/*
+
+/**
 Plugin Name: Drafts for Friends
 Plugin URI: http://automattic.com/
 Description: Now you don't need to add friends as users to the blog in order to let them preview your drafts
 Author: Neville Longbottom
 Version: 2.2
 Author URI:
-*/
+ */
 
 class DraftsForFriends {
 
-	function __construct() {
+	/**
+	 * Drafts For Friends constructor.
+	 */
+	public function __construct() {
 		add_action( 'init', array( &$this, 'init' ) );
 	}
 
-	function init() {
+	/**
+	 * Initialize the plugin.
+	 *
+	 * @return void
+	 */
+	public function init() {
 		global $current_user;
 		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
 		add_filter( 'the_posts', array( $this, 'the_posts_intercept' ) );
@@ -29,18 +38,33 @@ class DraftsForFriends {
 		$this->admin_page_init();
 	}
 
-	function admin_page_init() {
+	/**
+	 * Add admin scripts and styles.
+	 *
+	 * @return void
+	 */
+	public function admin_page_init() {
 		wp_enqueue_script( 'jquery' );
 		add_action( 'admin_head', array( $this, 'print_admin_css' ) );
 		add_action( 'admin_head', array( $this, 'print_admin_js' ) );
 	}
 
-	function get_admin_options() {
+	/**
+	 * Get the admin options for all shared objects.
+	 *
+	 * @return array Array with the shared objects
+	 */
+	public function get_admin_options() {
 		$saved_options = get_option( 'shared' );
 		return is_array( $saved_options ) ? $saved_options : array();
 	}
 
-	function save_admin_options() {
+	/**
+	 * Save the shared objects.
+	 *
+	 * @return void
+	 */
+	public function save_admin_options() {
 		global $current_user;
 		if ( $current_user->id > 0 ) {
 			$this->admin_options[ $current_user->id ] = $this->user_options;
@@ -48,14 +72,25 @@ class DraftsForFriends {
 		update_option( 'shared', $this->admin_options );
 	}
 
-	function add_admin_pages() {
+	/**
+	 * Add the admin page.
+	 *
+	 * @return void
+	 */
+	public function add_admin_pages() {
 		add_submenu_page(
 			'edit.php', __( 'Drafts for Friends', 'draftsforfriends' ), __( 'Drafts for Friends', 'draftsforfriends' ),
 			1, __FILE__, array( $this, 'output_existing_menu_sub_admin_page' )
 		);
 	}
 
-	function calc( $params ) {
+	/**
+	 * Calculate the expiration date for a new draft.
+	 *
+	 * @param array $params Array of shared draft options.
+	 * @return string String representation of the expiration date
+	 */
+	public function calc( $params ) {
 		$exp      = 60;
 		$multiply = 60;
 		if ( isset( $params['expires'] ) && ( $e = intval( $params['expires'] ) ) ) {
@@ -73,7 +108,13 @@ class DraftsForFriends {
 		return $exp * $multiply;
 	}
 
-	function process_post_options( $params ) {
+	/**
+	 * Process post options to set as new draft.
+	 *
+	 * @param array $params Array of shared draft options.
+	 * @return string A string saying why the post wasn't shared or a new line in the table will be displayed
+	 */
+	public function process_post_options( $params ) {
 		global $current_user;
 		if ( $params['post_id'] ) {
 			$p = get_post( $params['post_id'] );
@@ -92,7 +133,14 @@ class DraftsForFriends {
 		}
 	}
 
-	function process_delete( $params ) {
+	/**
+	 * Delete the shared post.
+	 *
+	 * @param array $params Array of shared draft options.
+	 * @return void Remove the shared post from the list
+	 */
+	public function process_delete( $params ) {
+		// TODO: delete operation not working for expired shared posts.
 		$shared = array();
 		foreach ( $this->user_options['shared'] as $share ) {
 			if ( $share['key'] == $params['key'] ) {
@@ -104,7 +152,14 @@ class DraftsForFriends {
 		$this->save_admin_options();
 	}
 
-	function process_extend( $params ) {
+	/**
+	 * Extend the shared post to have a new expiration date.
+	 *
+	 * @param array $params Array of shared draft options.
+	 * @return void Update the expiration date on the correspondent line in the list for the shared post
+	 */
+	public function process_extend( $params ) {
+		// TODO: doesn't seems to be working.
 		$shared = array();
 		foreach ( $this->user_options['shared'] as $share ) {
 			if ( $share['key'] == $params['key'] ) {
@@ -116,7 +171,12 @@ class DraftsForFriends {
 		$this->save_admin_options();
 	}
 
-	function get_drafts() {
+	/**
+	 * Get posts that can added as drafts (drafts, scheduled and pending review posts).
+	 *
+	 * @return array Array with an array for each type of posts that can be added as drafts
+	 */
+	public function get_drafts() {
 		global $current_user;
 		$my_drafts    = get_users_drafts( $current_user->id );
 		$my_scheduled = $this->get_users_future( $current_user->id );
@@ -141,12 +201,24 @@ class DraftsForFriends {
 		return $ds;
 	}
 
-	function get_users_future( $user_id ) {
+	/**
+	 * Get users posts marked as drafts.
+	 *
+	 * @param int $user_id User id.
+	 * @return array Array with all users post drafts
+	 */
+	public function get_users_future( $user_id ) {
+		// TODO: change the query.
 		global $wpdb;
 		return $wpdb->get_results( "SELECT ID, post_title FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'future' AND post_author = $user_id ORDER BY post_modified DESC" );
 	}
 
-	function get_shared() {
+	/**
+	 * Get the user shared posts.
+	 *
+	 * @return array Array with the user shared posts
+	 */
+	public function get_shared() {
 		return $this->user_options['shared'];
 	}
 
@@ -164,7 +236,12 @@ class DraftsForFriends {
 		}
 	}
 
-	function output_existing_menu_sub_admin_page() {
+	/**
+	 * Output the admin page.
+	 *
+	 * @return void
+	 */
+	public function output_existing_menu_sub_admin_page() {
 		if ( $_POST['draftsforfriends_submit'] ) {
 			$t = $this->process_post_options( $_POST );
 		} elseif ( $_POST['action'] == 'extend' ) {
@@ -275,7 +352,13 @@ foreach ( $dt[2] as $d ) :
 <?php
 	}
 
-	function can_view( $pid ) {
+	/**
+	 * Check if a friend can view the post.
+	 *
+	 * @param int $pid The post id.
+	 * @return boolean True if the url matches, false otherwise
+	 */
+	public function can_view( $pid ) {
 		foreach ( $this->admin_options as $option ) {
 			$shares = $option['shared'];
 			foreach ( $shares as $share ) {
@@ -287,7 +370,13 @@ foreach ( $dt[2] as $d ) :
 		return false;
 	}
 
-	function posts_results_intercept( $pp ) {
+	/**
+	 * If the post isn't published, and the friend can view, show it.
+	 *
+	 * @param array $pp Array of posts to be presented.
+	 * @return array Array with the same posts that was passed as a parameter
+	 */
+	public function posts_results_intercept( $pp ) {
 		if ( 1 != count( $pp ) ) {
 			return $pp;
 		}
@@ -299,7 +388,13 @@ foreach ( $dt[2] as $d ) :
 		return $pp;
 	}
 
-	function the_posts_intercept( $pp ) {
+	/**
+	 * Check if the post is a shared post, and return an array with that post.
+	 *
+	 * @param object $pp Object with information of the post.
+	 * @return array The post presented as an array
+	 */
+	public function the_posts_intercept( $pp ) {
 		if ( empty( $pp ) && ! is_null( $this->shared_post ) ) {
 			return array( $this->shared_post );
 		} else {
@@ -308,7 +403,13 @@ foreach ( $dt[2] as $d ) :
 		}
 	}
 
-	function tmpl_measure_select() {
+	/**
+	 * Template for the measure select.
+	 *
+	 * @return string String that represents the select dropdown
+	 */
+	public function tmpl_measure_select() {
+		// TODO: put this output better.
 		$secs  = __( 'seconds', 'draftsforfriends' );
 		$mins  = __( 'minutes', 'draftsforfriends' );
 		$hours = __( 'hours', 'draftsforfriends' );
@@ -324,7 +425,13 @@ foreach ( $dt[2] as $d ) :
 SELECT;
 	}
 
-	function print_admin_css() {
+	/**
+	 * Print the block for the CSS.
+	 *
+	 * @return void
+	 */
+	public function print_admin_css() {
+		// TODO: put the CSS as a seperated file.
 ?>
 	<style type="text/css">
 		a.draftsforfriends-extend, a.draftsforfriends-extend-cancel { display: none; }
@@ -335,7 +442,13 @@ SELECT;
 <?php
 	}
 
-	function print_admin_js() {
+	/**
+	 * Print the javascript block.
+	 *
+	 * @return void
+	 */
+	public function print_admin_js() {
+		// TODO: put the JS as seperated file.
 ?>
 	<script type="text/javascript">
 	jQuery(function() {
